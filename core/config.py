86df6ede8a45da2ebecc1796e57bc504b59998fb
CoderboticsBot <coderbotics@gmail.com>
2024-06-10 18:13:35 +0530

    name changes

--- core/config.py
+++ core/config.py
@@ -5,7 +5,7 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     APP_NAME: str = os.getenv('APP_NAME','Inventory')
-    SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL","postgresql://postgres:root@localhost/Inventory")
+    SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL","postgresql://postgres:d2webtech@localhost/Inventory")
     SECRET_KEY: str = os.getenv('SECRET_KEY','1f4cd5d9-504f-443e-9f85-181a1ed230d0')
     REFRESH_SECRET_KEY: str = os.getenv('REFRESH_SECRET_KEY','1f4cd5d9-504f-443e-9f85-181a1ed230d0')
     ALGORITHM: str = os.getenv('ALGORITHM','HS256')
